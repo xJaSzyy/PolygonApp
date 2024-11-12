0013a0db41679d7c3b62146f4643c460cbb945ee
Novoselov Stepan <novosel.stepan@gmail.com>
2024-11-12 15:22:24 +0700

    Add auto migration

--- backend/Program.cs
+++ backend/Program.cs
@@ -25,6 +25,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.Urls.Add("http://*:5000");  
 
 if (app.Environment.IsDevelopment())
